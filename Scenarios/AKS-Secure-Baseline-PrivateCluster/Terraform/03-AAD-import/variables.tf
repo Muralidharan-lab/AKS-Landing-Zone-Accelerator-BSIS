b79427004de818cc57bc5dacccf247ec4216d4a5
Muralidharan-lab <63875844+Muralidharan-lab@users.noreply.github.com>
2024-08-08 12:37:18 +0530

    Update variables.tf

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/variables.tf
@@ -10,7 +10,3 @@ variable "aks_user_group" {
   default = "AKS App Dev Team"
 }
 
-variable "arm_client_id" {}
-variable "arm_client_secret" {}
-variable "arm_tenant_id" {}
-variable "arm_subscription_id" {}
