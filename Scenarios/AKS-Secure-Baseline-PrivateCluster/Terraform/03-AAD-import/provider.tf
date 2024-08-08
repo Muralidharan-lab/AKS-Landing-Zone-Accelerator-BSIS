7ce3db22a981c018062eb70552e54805d3029143
Muralidharan-lab <63875844+Muralidharan-lab@users.noreply.github.com>
2024-08-08 10:31:53 +0530

    Update provider.tf

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
@@ -7,14 +7,19 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "= 3.44.1"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "= 2.34.1"
+    }
   }
 
   backend "azurerm" {
-    # resource_group_name  = ""   # Partial configuration, provided during "terraform init"
-    # storage_account_name = ""   # Partial configuration, provided during "terraform init"
-    # container_name       = ""   # Partial configuration, provided during "terraform init"
-    key                  = "aad"
+    resource_group_name  = "k8s"   # Partial configuration, provided during "terraform init"
+    storage_account_name = "k8stestbsi"   # Partial configuration, provided during "terraform init"
+     container_name       = "k8stestbsi"   # Partial configuration, provided during "terraform init"
+    key = "aad"
   }
+
 }
 
 provider "azurerm" {
